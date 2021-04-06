9d7cc9bb1ea1df7dbfe885e8b512e5931ddb13e5
Sanele Mpangalala <sanelem@fingerprintsoft.org>
2021-04-06 22:58:18 +0200

    added seeder for posts and users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+        [
+            'name' => 'John Doe',
+            'email' => 'john.doe@gmail.com',
+            'password' => Hash::make('password'),
+        ],
+        [
+            'name' => 'Mary Jane',
+            'email' => 'mary.jane@gmail.com',
+            'password' => Hash::make('password'),
+        ]
+        ]);
+
+        DB::table('posts')->insert(
+        [
+            [
+                'title' => 'Post 1',
+                'content' => 'This is post #1',
+                'user_id' => 1
+            ],
+            [
+                'title' => 'Post 2',
+                'content' => 'This is post #2',
+                'user_id' => 1
+            ],
+            [
+                'title' => 'Post 3',
+                'content' => 'This is post #3',
+                'user_id' => 2
+            ]
+        ]);
     }
 }
